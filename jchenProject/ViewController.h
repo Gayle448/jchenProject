9cf57d71cfdbfc54b1c1087f8a7b21fe8171c07d
惟楚有才 <zz@misschensmac-mini.local>
2018-03-09 17:09:06 +0800

    Initial Commit

---
+++ jchenProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jchenProject
+//
+//  Created by 惟楚有才 on 2018/3/9.
+//  Copyright © 2018年 Miss.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jchenProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jchenProject
+//
+//  Created by 惟楚有才 on 2018/3/9.
+//  Copyright © 2018年 Miss.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
